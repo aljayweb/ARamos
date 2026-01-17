76c17fabe5b4960bc1c4388cb104cf8fcaebbe4a
aljayweb <aljayramosll@gmail.com>
2026-01-17 12:54:19 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-document.getElementById("contact-form").addEventListener("submit", function(e) {
-    e.preventDefault(); // Prevent form from submitting the default way
-
-    const templateParams = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value,
-        message: document.getElementById("attachment").value,
-    };
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault(); // Stop normal form submit
 
     const serviceID = "service_55kjfra";
     const templateID = "template_m7m5f2a";
 
-    emailjs.send(serviceID, templateID, templateParams)
-    .then(response => {
-        alert("Your message was sent successfully!");
-        document.getElementById("contact-form").reset();
-        console.log(response);
-    })
-    .catch(error => {
-        console.error("FAILED...", error);
-        alert("Failed to send the message. Please try again.");
-    });
+    emailjs.sendForm(serviceID, templateID, this)
+        .then((response) => {
+            alert("Your image and message were sent successfully!");
+            this.reset();
+            console.log("SUCCESS!", response.status, response.text);
+        })
+        .catch((error) => {
+            console.error("FAILED...", error);
+            alert("Failed to send the message. Please try again.");
+        });
 });
